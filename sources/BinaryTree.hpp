91d40f96c264f4dc6264cbbfcf7aa324fdbc8dfe
ShmuelLa <shmuel516@gmail.com>
2021-06-06 17:25:26 +0300

    basic add left/right implementation - grade 60

--- sources/BinaryTree.hpp
+++ sources/BinaryTree.hpp
@@ -11,7 +11,10 @@
 
 #pragma once
 #include <iostream>
+#include <vector>
 using std::ostream;
+using std::vector;
+using std::endl;
 
 namespace ariel {
 
@@ -25,10 +28,10 @@ namespace ariel {
             Node *_right;
             Node *_left;
             T _content;
-            Node(T content) : _content(content), _parent(NULL), _right(NULL), _left(NULL) {};
+            Node(T content) : _content(content), _parent(nullptr), _right(nullptr), _left(nullptr) {};
         };
 
-        Node *_root;
+        Node *_root = nullptr;
 
         public:
             class Preorder_iterator {
@@ -40,18 +43,23 @@ namespace ariel {
                     T& operator*() const {
                         return _current->_content;
                     }
+
                     T* operator->() const {
                         return &(_current->_content);
                     }
+
                     Preorder_iterator& operator++() {
                         return *this;
                     }
+
                     const Preorder_iterator& operator++(int) {
                         return *this;
                     }
+
                     bool operator==(const Preorder_iterator& other) const {
                         return false;
                     }
+
                     bool operator!=(const Preorder_iterator& rhs) const {
                         return false;
                     }
@@ -66,18 +74,23 @@ namespace ariel {
                     T& operator*() const {
                         return _current->_content;
                     }
+
                     T* operator->() const {
                         return &(_current->_content);
                     }
+
                     Inorder_iterator& operator++() {
                         return *this;
                     }
+
                     const Inorder_iterator& operator++(int) {
                         return *this;
                     }
+
                     bool operator==(const Inorder_iterator& other) const {
                         return false;
                     }
+
                     bool operator!=(const Inorder_iterator& rhs) const {
                         return false;
                     }
@@ -92,58 +105,120 @@ namespace ariel {
                     T& operator*() const {
                         return _current->_content;
                     }
+
                     T* operator->() const {
                         return &(_current->_content);
                     }
+
                     Postorder_iterator& operator++() {
                         return *this;
                     }
+
                     const Postorder_iterator& operator++(int) {
                         return *this;
                     }
+
                     bool operator==(const Postorder_iterator& other) const {
                         return false;
                     }
+
                     bool operator!=(const Postorder_iterator& rhs) const {
                         return false;
                     }
             };
 
             BinaryTree() : _root(nullptr) {}
+
+            /**
+             * @brief recursively scans for a specific node in the tree on all left and right nodes
+             * this function will be call each time when trying to add a new node
+             * 
+             * @param parent_content the content of the node to scan, usually a parent to add child to
+             * @param current_node the current node the function is pointing at
+             * @return Node* the node found, null pointer otherwise
+             */
+            Node* locate_node(const T &parent_content, Node *current_node) const {
+                if (current_node == nullptr) {
+                    return nullptr;
+                }
+                if (current_node->_content == parent_content) {
+                    return current_node;
+                }
+                Node *scanner = this->locate_node(parent_content, current_node->_right);
+                if (scanner != nullptr) {
+                    return scanner;
+                }
+                return locate_node(parent_content, current_node->_left);
+            }
+
             BinaryTree& add_root(const T & num) {
-                this->_root = new Node(num);
+                if (this->_root == nullptr) {
+                    this->_root = new Node(num);
+                    return *this;
+                }
+                this->_root->_content = num;
                 return *this;
             }
-            BinaryTree<T> & add_left(const T & parent, const T & num) {
+
+            BinaryTree<T> & add_left(const T & parent_content, const T & num) {
+                Node* parent = locate_node(parent_content, this->_root);
+                if (parent == nullptr) {
+                    throw("Parent not found in tree");
+                }
+                if (parent->_left == nullptr) {
+                    parent->_left = new Node(num);
+                    return *this;
+                }
+                parent->_left->_content = num;
                 return *this;
             }
-            BinaryTree<T> & add_right(const T & parent, const T & num) {
+
+            BinaryTree<T> & add_right(const T & parent_content, const T & num) {
+                Node* parent = locate_node(parent_content, this->_root);
+                if (parent == nullptr) {
+                    throw("Parent not found in tree");
+                }
+                if (parent->_right == nullptr) {
+                    parent->_right = new Node(num);
+                    return *this;
+                }
+                parent->_right->_content = num;
                 return *this;
             }
+
             friend ostream &operator<<(ostream &stream, const BinaryTree<T> &tree) {
+                stream << "Tree" << endl;
                 return stream;
             }
+
             Inorder_iterator begin() {
                 return Inorder_iterator(_root);
             }
+
             Inorder_iterator end() {
                 return Inorder_iterator(nullptr);
             }
+
             Preorder_iterator begin_preorder() {
                 return Preorder_iterator(_root);
             }
+
             Preorder_iterator end_preorder() {
                 return Preorder_iterator(nullptr);
             }
+
             Inorder_iterator begin_inorder() {
                 return Inorder_iterator(_root);
             }
+
             Inorder_iterator end_inorder() {
                 return Inorder_iterator(nullptr);
             }
+
             Postorder_iterator begin_postorder() {
                 return Postorder_iterator(nullptr);
             }
+
             Postorder_iterator end_postorder() {
                 return Postorder_iterator(_root);
             }
